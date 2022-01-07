8f6ad2fb12e1c0157d72b43929c411e1d1e8872a
Minemu <44996819+Minemu398@users.noreply.github.com>
2022-01-07 21:48:02 +0900

    Update index.js

--- locales/index.js
+++ locales/index.js
@@ -16,6 +16,7 @@ const merge = (...args) => args.reduce((a, c) => ({
 const languages = [
 	'en-US',
 	'ja-JP',
+	'ja-OK',
 ];
 
 const primaries = {
@@ -30,6 +31,7 @@ module.exports = Object.entries(locales)
 		const [lang] = k.split('-');
 		switch (k) {
 			case 'ja-JP': return v;
+			case 'ja-OK': 
 			case 'en-US': return merge(locales['ja-JP'], v);
 			default: return merge(
 				locales['ja-JP'],
