fb306ea9fc3ee6c45a2cc60b2e7a008c4d1c46b1
atsu1125 <atsuchan@atsuchan.page>
2022-09-12 15:01:27 +0900

    require node v16 or newer

--- src/boot/master.ts
+++ src/boot/master.ts
@@ -89,7 +89,7 @@ export async function masterMain() {
 }
 
 const runningNodejsVersion = process.version.slice(1).split('.').map(x => parseInt(x, 10));
-const requiredNodejsVersion = [11, 7, 0];
+const requiredNodejsVersion = [16, 0, 0];
 const satisfyNodejsVersion = !lessThan(runningNodejsVersion, requiredNodejsVersion);
 
 function isWellKnownPort(port: number): boolean {
