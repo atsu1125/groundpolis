ca418fa75f0333cb7d46f9d276f4daf061c75e30
atsu1125 <atsuchan@atsuchan.page>
2022-09-13 18:08:58 +0900

    fix: API note

---
+++ src/server/api/endpoints/channels/pin-note.ts
@@ -0,0 +1,17 @@
+import define from '../../define';
+
+export const meta = {
+	requireCredential: true as const,
+
+	desc: {
+	},
+
+	tags: ['channels'],
+
+	params: {
+	},
+};
+
+export default define(meta, async (ps) => {
+	return;
+});
