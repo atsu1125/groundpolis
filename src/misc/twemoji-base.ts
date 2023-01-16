a51e1a1d6cf0cbcd1387f652cad289edf9fb9a87
atsu1125 <atsuchan@atsuchan.page>
2023-01-16 23:38:09 +0900

    Unicode Emoji 15.0

--- src/misc/twemoji-base.ts
+++ src/misc/twemoji-base.ts
@@ -1,1 +1,5 @@
-export const twemojiSvgBase = 'https://twemoji.maxcdn.com/v/latest/svg';
+// Self hosting
+//export const twemojiSvgBase = '/twemoji';
+
+// CDN
+export const twemojiSvgBase = 'https://cdn.jsdelivr.net/gh/mei23/memoji@15.0.0/dist/svg';
