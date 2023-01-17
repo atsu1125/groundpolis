c323ef91ed9c63e91f19233752e069242219d187
atsu1125 <atsuchan@atsuchan.page>
2023-01-17 19:19:38 +0900

    Hide Explore button if disabled

--- src/client/pages/welcome.entrance.a.vue
+++ src/client/pages/welcome.entrance.a.vue
@@ -30,7 +30,7 @@
 				<div class="action">
 					<MkButton @click="signup()" inline primary v-if="meta && !(meta.disableRegistration && meta.disableInvitation)">{{ $ts.signup }}</MkButton>
 					<MkButton @click="signin()" inline>{{ $ts.login }}</MkButton>
-					<MkButton inline style="margin-left: 12px;" onclick="window.location.href='/explore'">{{ $ts.explore }}</MkButton>
+					<MkButton inline style="margin-left: 12px;" onclick="window.location.href='/explore'" v-if="meta && !(meta.disableProfileDirectory)">{{ $ts.explore }}</MkButton>
 				</div>
 				<div class="status" v-if="onlineUsersCount && stats">
 					<div>
